1cd7a64e01dca9748b033102b14f203ac4e84f01
Paul Le Grand <paul@paullegrand.org>
2019-03-08 12:05:17 -0500

    Implement express for Heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+// server.js
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+var port = process.env.PORT || 5000
+
+app = express()
+app.use(serveStatic(__dirname + "/dist"))
+app.listen(port)
+
+console.log('server started '+ port)
