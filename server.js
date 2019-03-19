dd085b55edf80a567795c855213f3de5084dd4cc
Paul Le Grand <paul@paullegrand.org>
2019-03-19 11:55:53 -0400

    Initial move to express with api setup

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
 // server.js
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-var port = process.env.PORT || 5000
-
-app = express()
-app.use(serveStatic(__dirname + "/dist"))
-app.listen(port)
-
-console.log('server started '+ port)
+var app = require('./server/config/app');
+var server = app.start();
--- src/main.js
+++ src/main.js
@@ -1,10 +1,45 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import './sass/main.scss'
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
-Vue.config.productionTip = false
+import VueAxios from 'vue-axios';
+import axios from 'axios';
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.use(VueAxios, axios);
+
+Vue.config.productionTip = false;
+
+import HomeComponent from './components/HomeComponent.vue';
+import CreateComponent from './components/CreateComponent.vue';
+import IndexComponent from './components/IndexComponent.vue';
+import EditComponent from './components/EditComponent.vue';
+
+const routes = [
+  {
+      name: 'home',
+      path: '/',
+      component: HomeComponent
+  },
+  {
+      name: 'create',
+      path: '/create',
+      component: CreateComponent
+  },
+  {
+      name: 'posts',
+      path: '/posts',
+      component: IndexComponent
+  },
+  {
+      name: 'edit',
+      path: '/edit/:id',
+      component: EditComponent
+  }
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
