0cfa756edcbaeb239df2b27fcd488dee33a7cc37
Paul Le Grand <paul@paullegrand.org>
2019-03-29 15:02:12 -0400

    Set up example vue, with talking between front end and back end

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,98 @@
 <template>
-  <div class="container">
+<div class="container">
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-link to="/login" class="nav-link">Login</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/create" class="nav-link">Make Account</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/posts" class="nav-link">Posts</router-link>
-        </li>
-      </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <router-link to="/login" class="nav-link">Login</router-link>
+            </li>
+
+            <li class="nav-item">
+                <router-link to="/create" class="nav-link">Make Account</router-link>
+            </li>
+
+            <li class="nav-item">
+                <router-link to="/posts" class="nav-link">Posts</router-link>
+            </li>
+        </ul>
     </nav><br />
+
     <transition name="fade">
-      <router-view></router-view>
+        <router-view></router-view>
     </transition>
-  </div>
+
+    <button class="btn btn-primary" @click="getTweets">Click me to get tweets!</button>
+
+    <div v-if="hasTweets">
+        Woah, there are tweets now! Cool.
+
+        <ul>
+            <li v-for="tweet in tweets.statuses"> {{ tweet.possibly_sensitive }} </li>
+        </ul>
+    </div>
+    
+</div>
 </template>
 
 <style>
-    .fade-enter-active, .fade-leave-active {
-      transition: opacity .5s
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity .5s
     }
-    .fade-enter, .fade-leave-active {
-      opacity: 0
+
+    .fade-enter,
+    .fade-leave-active {
+        opacity: 0
     }
 </style>
 
 <script>
+export default 
+{
+    mixins: [],
 
-    export default{
+    props: 
+    {
+        // ...
+    },
+
+    data() 
+    {
+        return {
+            tweets: null,
+        }
+    },
+
+    computed:
+    {
+        hasTweets()
+        {
+            return this.tweets != null
+        },
+    },
+
+    mounted()
+    {
+        // ...
+    },
+
+    methods: 
+    {
+        // ...
+        getTweets()
+        {
+            const self = this
+
+            // @TODO: make this go to the api correctly
+            this.$http.get('//localhost:4000/twitter/getTweets').then(function (response)
+            {
+                if (response.status == "200")
+                {
+                    console.log(response)
+
+                    self.tweets = response.data
+                }
+          })
+        },
     }
+}
 </script>
