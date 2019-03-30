5967a4ed4136e13c5e220506c679de31c18b81dd
Paul Le Grand <paul@paullegrand.org>
2019-03-30 15:04:41 -0400

    Some login stuff

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,6 @@
 <div class="container">
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <ul class="navbar-nav">
-            <!-- <li class="nav-item">
-                <router-link to="/login" class="nav-link">Login</router-link>
-            </li>
-
-            <li class="nav-item">
-                <router-link to="/create" class="nav-link">Make Account</router-link>
-            </li> -->
             <li class ="nav-item">
               <router-link to="/" class="nav-link"> Home Page </router-link>
             </li>
@@ -21,87 +14,51 @@
     <transition name="fade">
         <router-view></router-view>
     </transition>
-</div>
-    <!-- <div id="fullscreen_bg" class="fullscreen_bg"/> -->
-        <div id="regContainer" class="container">
-            <div class="row">
-                <div class="col-md-6 col-md-offset-3">
-                    <div class ="panel panel login">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-6">
-                                    <a href="#" id="register-form-link">Register</a>
-                                </div>
-                            </div>
-                            <hr>
-                        </div>
 
-                        <div class ="panel-body">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <form id="login-form" action="#" method="post" role="form" style="display:block;">
-                                        <div class="form-group">
-                                            <label for="email">Email</label>
-                                            <input type="text" name ="email" id="email" tabindex="1" class="form-control" placeholder="Email" value="">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="password">Password</label>
-                                            <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
-                                        </div>
-                                        <div class="form-group text-center">
-                                            <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
-                                            <label for="remember">Remember Me</label>
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="row">
-                                                <div class="col-sm-6 col-sm-offset-3">
-                                                    <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In">
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
-                                    <form id="register-form" action="#" method="post" role="form" style="display: none;">
-                                        <div class="form-group">
-                                            <label for="email">Email</label>
-                                            <input type="text" name="email" id="email"tabindex="1" class="form-control" placeholder="Email" value="">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="password">Password</label>
-                                            <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="row">
-                                                <div class="col-sm-6 col-sm-offset-3"?
-                                                    <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+    <h1>Login</h1>
+
+    <div class="row">
+        <div class="col-xs-6">
+            <a @click="showLogin">Log in</a>
+        </div>
+
+        <div class="col-xs-6">
+            <a @click="showRegister">Register</a>
+        </div>
+    </div>
+    <form @submit.prevent="validateAccount">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                  <label>Email:</label>
+                  <input type="text" class="form-control" v-model="account.email">
+              </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label>Password:</label>
+                    <input type="password" class="form-control" v-model="account.password">
                 </div>
             </div>
         </div>
 
+        <div class="row" v-show="displayRegister">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label>Confirm password:</label>
+                    <input type="password" class="form-control" v-model="account.confirmPassword">
+                </div>
+            </div>
+        </div>
 
-
-
-
-    <!-- <button class="btn btn-primary" @click="getTweets">Click me to get tweets!</button>
-
-    <div v-if="hasTweets">
-        Woah, there are tweets now! Cool.
-
-        <ul>
-            <li v-for="tweet in safeTweets" :key="tweet.id">
-                {{ tweet.text }}
-            </li>
-        </ul>
-    </div> -->
-
-
+        <div class="form-group">
+            <button class="btn btn-primary">Login</button>
+        </div>
+    </form>
+</div>
 </template>
 
 <style>
@@ -129,7 +86,10 @@ export default
     data()
     {
         return {
-            tweets: null,
+            tweets:         null,
+            account:        {},
+            displayLogin:   true,
+            displayRegister:false,
         }
     },
 
@@ -176,6 +136,18 @@ export default
                     self.tweets = response.data
                 }
           })
+        },
+
+        showLogin()
+        {
+            this.displayLogin = true
+            this.displayRegister = false
+        },
+
+        showRegister()
+        {
+            this.displayLogin = false
+            this.displayRegister = true
         },
     }
 }
--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,44 +1,27 @@
 <template>
-  <div>
-    <h1>Login</h1>
-    <form @submit.prevent="validateAccount">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="form-group">
-            <label>Email:</label>
-            <input type="text" class="form-control" v-model="account.email">
-          </div>
-        </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6">
-            <div class="form-group">
-               <label>Password:</label>
-            <input type="text" class="form-control" v-model="account.password">
-          </div>
-          </div>
-        </div><br />
-        <div class="form-group">
-          <button class="btn btn-primary">Login</button>
-        </div>
-    </form>
-  </div>
+<div>
+    <!--  -->
+</div>
 </template>
 
 <script>
     export default {
-        data(){
-        return {
-          account:{}
-        }
-    },
-    methods: {
-      validateAccount(){
-        let uri = 'http://localhost:4000/accounts/validate';
-        this.axios.post(uri, this.account).then(() => {
-          this.$router.push({name: 'account'});
-        });
-      }
+        data() 
+    	{
+    		return {
+                // ...
+    		}
+    	},
+
+        methods: {
+            validateAccount()
+            {
+                let uri = 'http://localhost:4000/accounts/validate'
+
+                this.axios.post(uri, this.account).then(() => {
+                    this.$router.push({name: 'account'})
+                })
+            },
     }
   }
 </script>
