9ffdf45bbd649aad1dd875ed937485b2f496fc93
mirandacraghead <mirandacraghead@ufl.edu>
2019-03-30 14:46:17 -0400

    Some work I did

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
 <div class="container">
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <ul class="navbar-nav">
-            <li class="nav-item">
+            <!-- <li class="nav-item">
                 <router-link to="/login" class="nav-link">Login</router-link>
             </li>
 
             <li class="nav-item">
                 <router-link to="/create" class="nav-link">Make Account</router-link>
+            </li> -->
+            <li class ="nav-item">
+              <router-link to="/" class="nav-link"> Home Page </router-link>
             </li>
-
             <li class="nav-item">
                 <router-link to="/posts" class="nav-link">Posts</router-link>
             </li>
@@ -19,8 +21,75 @@
     <transition name="fade">
         <router-view></router-view>
     </transition>
+</div>
+    <!-- <div id="fullscreen_bg" class="fullscreen_bg"/> -->
+        <div id="regContainer" class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class ="panel panel login">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-6">
+                                    <a href="#" id="register-form-link">Register</a>
+                                </div>
+                            </div>
+                            <hr>
+                        </div>
 
-    <button class="btn btn-primary" @click="getTweets">Click me to get tweets!</button>
+                        <div class ="panel-body">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <form id="login-form" action="#" method="post" role="form" style="display:block;">
+                                        <div class="form-group">
+                                            <label for="email">Email</label>
+                                            <input type="text" name ="email" id="email" tabindex="1" class="form-control" placeholder="Email" value="">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="password">Password</label>
+                                            <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                                        </div>
+                                        <div class="form-group text-center">
+                                            <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
+                                            <label for="remember">Remember Me</label>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="row">
+                                                <div class="col-sm-6 col-sm-offset-3">
+                                                    <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <form id="register-form" action="#" method="post" role="form" style="display: none;">
+                                        <div class="form-group">
+                                            <label for="email">Email</label>
+                                            <input type="text" name="email" id="email"tabindex="1" class="form-control" placeholder="Email" value="">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="password">Password</label>
+                                            <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="row">
+                                                <div class="col-sm-6 col-sm-offset-3"?
+                                                    <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+    <!-- <button class="btn btn-primary" @click="getTweets">Click me to get tweets!</button>
 
     <div v-if="hasTweets">
         Woah, there are tweets now! Cool.
@@ -30,9 +99,9 @@
                 {{ tweet.text }}
             </li>
         </ul>
-    </div>
-    
-</div>
+    </div> -->
+
+
 </template>
 
 <style>
@@ -48,16 +117,16 @@
 </style>
 
 <script>
-export default 
+export default
 {
     mixins: [],
 
-    props: 
+    props:
     {
         // ...
     },
 
-    data() 
+    data()
     {
         return {
             tweets: null,
@@ -91,7 +160,7 @@ export default
         this.getTweets()
     },
 
-    methods: 
+    methods:
     {
         // ...
         getTweets()
