0907f1829ede7cdde48a86f3704a51375f1f9e7b
Paul Le Grand <paul@paullegrand.org>
2019-04-02 11:27:46 -0400

    Make logins dynamic

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -111,7 +111,7 @@ export default {
 
         createAccount()
         {
-            let uri = 'http://localhost:4000/accounts/add'
+            let uri = '/accounts/add'
 
             this.axios.post(uri, this.account).then(() => {
                 this.$router.push({
@@ -122,7 +122,7 @@ export default {
 
         LoginAccount()
         {
-            let uri = 'http://localhost:4000/accounts/Login'
+            let uri = '/accounts/Login'
 
             this.axios.post(uri, this.account).then(() => {
                 this.$router.push({name: 'account'})
