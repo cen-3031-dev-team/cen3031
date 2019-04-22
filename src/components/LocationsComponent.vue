60c8da0f2348bf66eeed50c3d891e5e3ffa054ab
Paul Le Grand <paul@paullegrand.org>
2019-04-22 18:55:35 -0400

    Initial addition of search for topics by location

---
+++ src/components/LocationsComponent.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="tweet-container">
+    <div class="tweet container" v-for="location in locations" :key="location.id">
+        <div class="row">
+            <div class="col-md-12">
+                <span>
+                    {{ location.name }}
+                </span>
+            </div>
+
+            <div class="col-md-12 tag">
+                Count: {{ location.tweet_volume }}
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+    props: ['locations'],
+
+    data() {
+        return {
+            // ...
+        }
+    },
+
+    computed:
+    {
+        // ...
+    },
+
+    methods: {
+        // ...
+    }
+}
+</script>
